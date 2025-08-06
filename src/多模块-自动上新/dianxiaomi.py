6aeffe35535e5ee81ac4b67150f5e85943e7d17c
cloud23333 <zxckjhdfg@gmail.com>
2025-08-06 22:37:43 +0800

    1

--- src/多模块-自动上新/dianxiaomi.py
+++ src/多模块-自动上新/dianxiaomi.py
@@ -1,28 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-import os, warnings, logging, time
-import config, common
-
+from selenium.webdriver import Chrome, ChromeOptions, ChromeService
+import os, warnings, logging, time, config, common
 
 def init_driver():
-    chrome_opts = Options()
-    chrome_opts.add_argument("--log-level=3")
-    chrome_opts.add_experimental_option("excludeSwitches", ["enable-logging"])
-    service = Service(log_path=os.devnull)
+    opts = ChromeOptions()
+    opts.binary_location = r"chrome\chrome.exe"
+    opts.add_argument(r"--user-data-dir=chrome\User Data")
+    opts.add_argument("--log-level=3")
+    opts.add_experimental_option("excludeSwitches", ["enable-logging"])
+    service = ChromeService(executable_path=r"chrome\chromedriver.exe", log_path=os.devnull)
     warnings.filterwarnings("ignore", category=UserWarning, module="pywinauto")
     logging.getLogger("absl").setLevel(logging.ERROR)
-    driver = webdriver.Chrome(service=service, options=chrome_opts)
-    return driver
-
+    return Chrome(service=service, options=opts)
 
 def login(driver):
     driver.get("https://www.dianxiaomi.com/")
     time.sleep(1)
-    common.wait_present(driver, '//*[@id="exampleInputName"]').send_keys(
-        config.USERNAME
-    )
-    common.wait_present(driver, '//*[@id="exampleInputPassword"]').send_keys(
-        config.PASSWORD
-    )
+    common.wait_present(driver, '//*[@id="exampleInputName"]').send_keys(config.USERNAME)
+    common.wait_present(driver, '//*[@id="exampleInputPassword"]').send_keys(config.PASSWORD)
     input("验证码后回车...")
