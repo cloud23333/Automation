3a41b0382bdeaac4ed64db0c6485192de35a5751
cloud23333 <zxckjhdfg@gmail.com>
2025-08-06 17:18:15 +0800

    1

--- src/多模块-自动上新/shopee_test.py
+++ src/多模块-自动上新/shopee_test.py
@@ -1,6 +1,6 @@
-from common import wait_click, wait_present
-import dianxiaomi, time
-import shopee   
+from common import wait_present
+import dianxiaomi
+import shopee_local as shopee
 import config
 
 def main():
@@ -10,12 +10,22 @@ def main():
         driver.get("https://www.dianxiaomi.com/web/shopeeGlobal/add")
         wait_present(driver, '//*[@id="basicInfo"]', timeout=15)
         import pandas as pd
-        df = pd.read_excel(config.SHOPEE_PRODUCTS_XLSX, engine="openpyxl")
+        df = pd.read_excel(
+            config.SHOPEE_PRODUCTS_XLSX,
+            sheet_name="shopee_product",
+            engine="openpyxl",
+        )
+        df_skus = pd.read_excel(
+            config.SHOPEE_PRODUCTS_XLSX,
+            sheet_name="shopee_images",
+            engine="openpyxl",
+        )
         row = df.iloc[0]
-        title = row['title']
-        desc = row['desc']
-        img_list = [row[c] for c in df.columns if c.startswith('img') and pd.notna(row[c])]
+        title = row["title"]
+        desc = row["desc"]
+        img_list = [row[c] for c in df.columns if c.startswith("img") and pd.notna(row[c])]
         shopee.choose_category(driver, title, desc, img_list)
+        shopee.fill_sku_table(driver, row["product_id"], df_skus)
         input("表单已填，回车结束测试...")
     finally:
         driver.quit()
